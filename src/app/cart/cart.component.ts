7921135bc3d7be32668676eb76f4be63d699028f
Yasser Bal <yasserbal@icloud.com>
2022-08-07 08:37:07 +0200

    Added Cart Component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
+
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,9 +12,21 @@ import { CartService } from '../cart.service';
 export class CartComponent {
 
   items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  })
   constructor(
-    private cartService:CartService
+    private cartService:CartService,
+    private formBuilder:FormBuilder
   ){
+
+  }
+  onSubmit():void{
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
 
   }
 }
